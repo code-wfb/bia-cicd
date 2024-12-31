26a3d6eeb7a68347a79957119949c41b392504f7
wallace <wallacewfb@gmail.com>
2024-12-31 18:35:22 -0300

    adicionando path do ecr

--- scripts/ecs/unix/build.sh
+++ scripts/ecs/unix/build.sh
@@ -1,5 +1,5 @@
 ECR_REGISTRY="SEU_REGISTRY"
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $ECR_REGISTRY
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 025066254773.dkr.ecr.us-east-1.amazonaws.com
 docker build -t bia .
-docker tag bia:latest $ECR_REGISTRY/bia:latest
-docker push $ECR_REGISTRY/bia:latest
+docker tag bia:latest 025066254773.dkr.ecr.us-east-1.amazonaws.com/bia:latest
+docker push 025066254773.dkr.ecr.us-east-1.amazonaws.com/bia:latest
